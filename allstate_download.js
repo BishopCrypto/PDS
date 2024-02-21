a8f09019c59bf6f64848c18c50f26419da84f96a
2-KIH\2-KIH <kingransom9411@gmail.com>
2024-02-22 08:57:51 +0900

    no message

--- allstate_download.js
+++ allstate_download.js
@@ -197,7 +197,7 @@ async function main() {
     const browser = await puppeteer.launch({
         headless: false,
         defaultViewport: null,
-        args: ['--disable-dev-shm-usage', '--single-process'],
+        args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage', '--single-process'],
         timeout: 0,
     });
 
