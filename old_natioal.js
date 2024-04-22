0af9f71e4b2167617cb4d56ede97ed0c3ba98948
Darian <kingransom9411@gmail.com>
2024-04-22 14:26:48 +0900

    headless

--- old_natioal.js
+++ old_natioal.js
@@ -74,7 +74,7 @@ async function getSMS(token) {
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     defaultViewport: null,
     devtools: false,
     args: [
