fb40ca179e7232a77258d2a0deda4250723704a2
Darian <kingransom9411@gmail.com>
2024-06-11 17:10:35 +0900

    restart

--- service_checking.js
+++ service_checking.js
@@ -10,11 +10,17 @@ exec(`sudo systemctl status ${serviceName}.service`, (error, stdout, stderr) => 
 
   const statusIndex = stdout.indexOf('Active:');
   if (statusIndex !== -1) {
-    const statusSection = stdout.substring(0, statusIndex + stdout.substring(statusIndex).indexOf('\n'));
-    console.log(stdout.trim());
-    console.log(`Service ${serviceName} is running successfully.`);
+    const statusSection = stdout.substring(0, statusIndex + stdout.substring(statusIndex).indexOf('\n'));        
     console.log(statusSection);
 
+    if (statusSection.indexOf('Active: active (running)') !== -1) {
+      console.log(`Service ${serviceName} is running successfully.`);
+    }
+    else {
+      console.log(`Service ${serviceName} is stopped.`);
+      exec(`sudo systemctl restart ${serviceName}.service`);
+      console.log('Service is restarted now but Please check again');
+    }
   } else {
     console.error('Error: Failed to retrieve service status information.');
   }
