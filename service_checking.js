829541159ef844f4cdfc36c383b3a4bd608ed3c7
Darian <kingransom9411@gmail.com>
2024-06-11 18:14:06 +0900

    resolve

--- service_checking.js
+++ service_checking.js
@@ -24,50 +24,65 @@ for (const serviceName of serviceNames) {
   let subject = '';
   let text = '';
 
-  exec(`sudo systemctl status ${serviceName}.service`, (error, stdout, stderr) => {
-    if (stderr) {
-      subject = `Service ${serviceName} error`;
-      text = `Command error: ${stderr}`;
-      console.error(text);
-      return;
-    }
-  
-    const statusIndex = stdout.indexOf('Active:');
-    if (statusIndex !== -1) {
-      const statusSection = stdout.substring(0, statusIndex + stdout.substring(statusIndex).indexOf('\n'));
-      console.log(statusSection);
-      text += statusSection + '\n\n';
-  
-      if (statusSection.indexOf('Active: active (running)') !== -1) {
-        subject = `Service ${serviceName} is running successfully.`
-        console.log(subject);
-      }
-      else {
-        subject = `Service ${serviceName} is stopped.`;
-        console.log(subject);
-        exec(`sudo systemctl restart ${serviceName}.service`);
-        console.log('Service is restarted now but Please check again');
-        text += 'Service is restarted now but Please check again';
-      }
-    } else {
-      subject = `Service ${serviceName} error`;
-      text = 'Error: Failed to retrieve service status information.';
-      console.error(text);
-    }
-  });
-  
-  mailOptions['subject'] = subject;
-  mailOptions['text'] = text;
-  for (const toEmail of ['kingransom9411@gmail.com', 'leo636722@gmail.com']) {
-    mailOptions['to'] = toEmail;
-    console.log(mailOptions);
-    transporter.sendMail(mailOptions, (error, info) => {
-      if (error) {
-        console.error(`Error sending email to ${toEmail}: ${error.message}`);
-      } else {
-        console.log(`Email notification sent to ${toEmail} successfully.`);
-      }
+  // Wrap the exec function in a Promise
+  const executeCommand = () => {
+    return new Promise((resolve, reject) => {
+      exec(`sudo systemctl status ${serviceName}.service`, (error, stdout, stderr) => {
+        if (stderr) {
+          subject = `Service ${serviceName} error`;
+          text = `Command error: ${stderr}`;
+          console.error(text);
+          reject();
+        }
+
+        const statusIndex = stdout.indexOf('Active:');
+        if (statusIndex !== -1) {
+          const statusSection = stdout.substring(0, statusIndex + stdout.substring(statusIndex).indexOf('\n'));
+          console.log(statusSection);
+          text += statusSection + '\n\n';
+
+          if (statusSection.indexOf('Active: active (running)') !== -1) {
+            subject = `Service ${serviceName} is running successfully.`
+            console.log(subject);
+          }
+          else {
+            subject = `Service ${serviceName} is stopped.`;
+            console.log(subject);
+            exec(`sudo systemctl restart ${serviceName}.service`);
+            console.log('Service is restarted now but Please check again');
+            text += 'Service is restarted now but Please check again';
+          }
+          resolve();
+        } else {
+          subject = `Service ${serviceName} error`;
+          text = 'Error: Failed to retrieve service status information.';
+          console.error(text);
+          reject();
+        }
+      });
     });
-    setTimeout(() => {}, 3000);
-  }
+  };
+
+  // Call the executeCommand function and wait for it to finish before sending the email
+  executeCommand()
+    .then(() => {
+      mailOptions['subject'] = subject;
+      mailOptions['text'] = text;
+      
+      for (const toEmail of ['kingransom9411@gmail.com', 'leo636722@gmail.com']) {
+        mailOptions['to'] = toEmail;
+        console.log(mailOptions);
+        
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.error(`Error sending email to ${toEmail}: ${error.message}`);
+          } else {
+            console.log(`Email notification sent to ${toEmail} successfully.`);
+          }
+        });
+      }
+    })
+    .catch(() => {
+      // Handle any errors here
+    });
 }
