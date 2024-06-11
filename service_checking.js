e0b0dd53de133ca3d96229c04ab7a1fb68e5b8c7
Darian <kingransom9411@gmail.com>
2024-06-11 17:52:10 +0900

    noti

--- service_checking.js
+++ service_checking.js
@@ -1,34 +1,7 @@
-// const { exec } = require('child_process');
-
-// const serviceName = 'emailservice';
-
-// exec(`sudo systemctl status ${serviceName}.service`, (error, stdout, stderr) => {
-//   if (stderr) {
-//     console.error(`Command error: ${stderr}`);
-//     return;
-//   }
-
-//   const statusIndex = stdout.indexOf('Active:');
-//   if (statusIndex !== -1) {
-//     const statusSection = stdout.substring(0, statusIndex + stdout.substring(statusIndex).indexOf('\n'));        
-//     console.log(statusSection);
-
-//     if (statusSection.indexOf('Active: active (running)') !== -1) {
-//       console.log(`Service ${serviceName} is running successfully.`);
-//     }
-//     else {
-//       console.log(`Service ${serviceName} is stopped.`);
-//       exec(`sudo systemctl restart ${serviceName}.service`);
-//       console.log('Service is restarted now but Please check again');
-//     }
-//   } else {
-//     console.error('Error: Failed to retrieve service status information.');
-//   }
-// });
-
+const { exec } = require('child_process');
 const nodemailer = require('nodemailer');
 
-const serviceName = 'emailservice';
+const serviceNames = ['emailservice', 'textservice'];
 const emailService = 'Gmail'; // Assuming you are using Gmail
 
 // Configure Nodemailer with the Gmail SMTP settings and your credentials
@@ -40,18 +13,59 @@ const transporter = nodemailer.createTransport({
   }
 });
 
-// Send email notification
-const mailOptions = {
-  from: 'accounts@thedevelopers.dev',
-  to: 'kingransom9411@gmail.com',
-  subject: `Service ${serviceName} is inactive`,
-  text: 'Please check service again'
-};
+for (const serviceName of serviceNames) {
+  // Send email notification
+  let mailOptions = {
+    from: 'accounts@thedevelopers.dev',
+    to: '',
+    subject: '',
+    text: ''
+  };
+  let subject = '';
+  let text = '';
 
-transporter.sendMail(mailOptions, (error, info) => {
-  if (error) {
-    console.error(`Error sending email: ${error.message}`);
-  } else {
-    console.log('Email notification sent successfully.');
+  exec(`sudo systemctl status ${serviceName}.service`, (error, stdout, stderr) => {
+    if (stderr) {
+      subject = `Service ${serviceName} error`;
+      text = `Command error: ${stderr}`;
+      console.error(text);
+      return;
+    }
+  
+    const statusIndex = stdout.indexOf('Active:');
+    if (statusIndex !== -1) {
+      const statusSection = stdout.substring(0, statusIndex + stdout.substring(statusIndex).indexOf('\n'));
+      console.log(statusSection);
+      text += statusSection + '\n\n';
+  
+      if (statusSection.indexOf('Active: active (running)') !== -1) {
+        subject = `Service ${serviceName} is running successfully.`
+        console.log(subject);
+      }
+      else {
+        subject = `Service ${serviceName} is stopped.`;
+        console.log(subject);
+        exec(`sudo systemctl restart ${serviceName}.service`);
+        console.log('Service is restarted now but Please check again');
+        text += 'Service is restarted now but Please check again';
+      }
+    } else {
+      subject = `Service ${serviceName} error`;
+      text = 'Error: Failed to retrieve service status information.';
+      console.error(text);
+    }
+  });
+  
+  mailOptions['subject'] = subject;
+  mailOptions['text'] = text;
+  for (const toEmail of ['kingransom9411@gmail.com', 'leo636722@gmail.com']) {
+    mailOptions['to'] = toEmail;
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error(`Error sending email to ${toEmail}: ${error.message}`);
+      } else {
+        console.log(`Email notification sent to ${toEmail} successfully.`);
+      }
+    });
   }
-});
+}
